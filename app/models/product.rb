b64c1da7fab377682766b9fd04c375985feaa9df
kizuchie <chichen_08@yahoo.com>
2012-04-06 16:14:00 +0800

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,14 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-  validates :title, :uniqueness => true, :length => {
-      :minimum => 10,
-      :message => 'must be at least 10 characters long.'}
+  validates :title, :uniqueness => true
   validates :image_url, :format => {
       :with => %r{\.(gif|jpg|png)$}i,
       :message => 'must be a URL for GIF, JPG or PNG image.'
   }
 end
+
+# :length => {
+# :minimum => 10,
+# :message => 'title must be at least 10 characters long''
+# }
